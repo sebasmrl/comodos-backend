8f961c0418f08edf110ed0d1e8659b74f2a7063b
Sebastian Morales <davesebastian99@gmail.com>
2025-08-05 21:31:18 -0500

    Modified: main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
   app.enableCors({ 
-    origin:[...(process.env.ENABLE_CORS ?? '').split(',')],
+    origin:[...(process.env.ENABLE_CORS ?? '').split(',').filter((value)=> value != '')],
     credentials:true 
   });
   app.useGlobalPipes(new ValidationPipe({
